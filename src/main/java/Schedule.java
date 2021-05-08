6584b5386b3f1d1e4bd388974ed88d5aa67cfda8
Ayrtonko <80459193+Ayrtonko@users.noreply.github.com>
2021-05-08 22:42:17 +0200

    Schedule class framework

---
+++ src/main/java/Schedule.java
@@ -0,0 +1,13 @@
+public class Schedule {
+    private String name;
+
+    public Schedule(String name){}
+
+    public String summary(){
+        return "";
+    }
+
+    public Schedule export(){
+        return this;
+    }
+}
